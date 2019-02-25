cfe78535c27556def01e456a2d7f3b6b793e2cb0
Ritesh SIngh <gettoriteshsingh@gmail.com>
2019-02-25 16:34:12 +0530

    sec commit

---
+++ cmake-build-debug/CMakeFiles/Coding_Pearls.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Coding_Pearls.dir/maxheap.cpp.obj"
+  "Coding_Pearls.pdb"
+  "Coding_Pearls.exe"
+  "Coding_Pearls.exe.manifest"
+  "libCoding_Pearls.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Coding_Pearls.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ maxheap.cpp
@@ -0,0 +1,11 @@
+//
+// Created by kumar on 25-02-2019.
+//
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    cout<<"hello world";
+}
